7c0b83f01e551db15a447d5acab6632daca46d94
Uni-wv <65159528+Uni-wv@users.noreply.github.com>
2020-07-28 23:48:52 +0530

    main program file

---
+++ main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:layout_app/ui/home.dart';
+
+main() {
+  runApp(app());
+}
+
+class app extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return myapp();
+  }
+}
